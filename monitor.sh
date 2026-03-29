2781077b3d1dac0c44ccf4f49d154a13e237a9a7
repen7ant <ilyarepentant@icloud.com>
2026-03-29 18:38:35 +0500

    group identical processes and fix cpu load

--- monitor.sh
+++ monitor.sh
@@ -43,13 +43,7 @@ init_log_dir() {
 }
 
 get_cpu_load() {
-    uptime |
-        awk -F'[,:]' '{
-            for (i=NF; i>=1; i--) {
-                if ($i ~ /[0-9]+\.[0-9]+/) { print $i; break }
-            }
-        }' |
-        awk '{print $1}'
+    top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}'
 }
 
 get_memory_usage() {
@@ -65,22 +59,41 @@ get_disk_usage() {
 }
 
 get_top_cpu() {
-    ps -eo %cpu,comm --sort=-%cpu |
-        awk 'NR>1 && $1>0 && $2!="ps" { printf "  %d. %s (%.1f%%)\n", ++n, $2, $1 }' |
-        head -5
+    ps -eo %cpu,comm |
+        awk 'NR>1 && $2 !~ /^(ps|awk|grep|bash)$/ { arr[$2] += $1 } 
+             END { for (i in arr) { if (arr[i] > 0.1) printf "%.1f %s\n", arr[i], i } }' |
+        sort -rnk1 |
+        head -5 |
+        awk '{ printf "  %d. %s (%.1f%%)\n", ++n, $2, $1 }'
 }
 
 get_top_mem() {
-    ps -eo rss,comm --sort=-rss |
-        awk 'NR>1 && $1>0 {
-            if ($1 >= 1048576)
-                printf "  %d. %s (%.1fGB)\n", ++n, $2, $1/1048576
-            else if ($1 >= 1024)
-                printf "  %d. %s (%.0fMB)\n", ++n, $2, $1/1024
-            else
-                printf "  %d. %s (%dKB)\n",   ++n, $2, $1
-          }' |
-        head -5
+    grep -s "^Pss:" /proc/[0-9]*/smaps_rollup | awk -F'[: ]+' '
+    {
+        split($1, path, "/")
+        pid = path[3]
+        
+        comm_file = "/proc/" pid "/comm"
+        if ((getline comm < comm_file) > 0) {
+            if (comm !~ /^(ps|awk|grep|bash|cat)$/) {
+                arr[comm] += $3
+            }
+        }
+        close(comm_file)
+    } 
+    END {
+        for (i in arr) {
+            if (arr[i] > 0) printf "%d %s\n", arr[i], i
+        }
+    }' |
+        sort -rnk1 |
+        head -5 |
+        awk '{
+        n++;
+        if ($1 >= 1048576) printf "  %d. %s (%.1fGB)\n", n, $2, $1/1048576
+        else if ($1 >= 1024) printf "  %d. %s (%.0fMB)\n", n, $2, $1/1024
+        else printf "  %d. %s (%dKB)\n", n, $2, $1
+    }'
 }
 
 build_report() {
